0796d497b4e23c5afa9b9de93eb3acecd6269953
Wind031013 <bnd1013@qq.com>
2025-09-10 11:47:36 +0800

    1.0

--- 代码/数据处理/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-logues:
-    #     persons, adjectives = find_name_adjective(dialogues)
-    #     # 列表中分别记录姓名，情感形容词，说的话
-    #     train_data.append({
-    #         'character': persons,
-    #         'emotion': adjectives,
-    #         'dialogue': dialogues
-    #     })
-    # return tra
